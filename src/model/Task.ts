98c5df1ff20916bea0a5d2d05cfedb0fb559b5d3
matheusezaquiel <matheusbezaquiel@gmail.com>
2024-05-12 19:14:06 -0300

    permiting null values on class

--- src/model/Task.ts
+++ src/model/Task.ts
@@ -1,19 +1,32 @@
 export class Task {
 
-    id: number;
-    title: string;
-    done: boolean;
-    description: string;
-    createdAt: Date;
-    updatedAt: Date;
+    id: number | null;
+    title: string | null;
+    done: boolean | null;
+    description: string | null;
+    state: number | null;
+    createdAt: Date | null;
+    updatedAt: Date | null;
 
-    constructor(id: number, title: string, description: string, done: boolean, createdAt: Date, updatedAt: Date) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.done = done;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
+    constructor(id?: number | null, title?: string, description?: string, done?: boolean, state?: number, createdAt?: Date, updatedAt?: Date) {
+
+        if(id !== undefined && title !== undefined && description !== undefined && done !== undefined && state !== undefined &&  createdAt !== undefined && updatedAt !== undefined) {
+            this.id = null;
+            this.title = title;
+            this.description = description;
+            this.done = done;
+            this.createdAt = createdAt;
+            this.updatedAt = updatedAt;
+        } 
+        
+        this.id = id || null;
+        this.title = title || null;
+        this.description = description || null;
+        this.done = done || null;
+        this.state = state || null;
+        this.createdAt = createdAt || null;
+        this.updatedAt = updatedAt || null;
+        
     }
 
 }
